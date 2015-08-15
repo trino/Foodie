1a588f46612c64008d8c91b74fc05af23d34adfc
Van <info@trinoweb.com>
2015-08-15 10:12:58 -0400

    layout

--- webroot/common/header.php
+++ webroot/common/header.php
@@ -24,8 +24,8 @@
 
     <!-- Fonts START -->
 
-    <!--link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|PT+Sans+Narrow|Source+Sans+Pro:200,300,400,600,700,900&amp;subset=all" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900&amp;subset=all" rel="stylesheet" type="text/css"-->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|PT+Sans+Narrow|Source+Sans+Pro:200,300,400,600,700,900&amp;subset=all" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900&amp;subset=all" rel="stylesheet" type="text/css">
 
     <!--- fonts for slider on the index page -->
     <!-- Fonts END -->
@@ -57,10 +57,10 @@
 
     <!-- BEGIN CORE PLUGINS (REQUIRED FOR ALL PAGES) -->
 
-
     <!--[if lt IE 9]>
     <script src="<?php echo $this->request->webroot;?>plugins/respond.min.js"></script>
     <![endif]-->
+
     <script src="<?php echo $this->request->webroot;?>plugins/jquery.min.js" type="text/javascript"></script>
     <script src="<?php echo $this->request->webroot;?>plugins/jquery-migrate.min.js" type="text/javascript"></script>
     <script src="<?php echo $this->request->webroot;?>plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
@@ -71,13 +71,9 @@
     <script src="<?php echo $this->request->webroot;?>scripts/upload.js"></script>
     
     <!-- END CORE PLUGINS -->
-
-
 </head>
 <!-- Head END -->
 <?php include_once("api.php"); ?>
-
-
 
 <!-- Body BEGIN -->
 <body class="ecommerce">
@@ -91,81 +87,84 @@
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 
-
 <!-- BEGIN TOP BAR -->
-<div class="pre-header">
-    <div class="container-fluid">
-        <div class="row">
-            <!-- BEGIN TOP BAR LEFT PART -->
-            <?php echo $this->element('mini_nav');?>
-            <!-- END TOP BAR MENU -->
-        </div>
-        <div id="login-pop-up" style="display:none;">
-            <div class="login-pop-up">
-                <div class="col-md-12 col-sm-12 col-xs-12">
-                    <div class="login-form">
-                        <h1>Login</h1>
-                        <DIV ID="message" align="center"></DIV>
-                        <form role="form" action="<?php echo $this->request->webroot;?>cuisine/login" method="post" class="form-horizontal form-without-legend">
-                            <input type="hidden" name="action" value="login">
-                            <div class="form-group">
-                                <label class="col-lg-4 control-label" for="email">Email <span class="require">*</span></label>
-                                <div class="col-lg-8">
-                                    <input type="text" id="email" name="email" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="col-lg-4 control-label" for="password">Password <span class="require">*</span></label>
-                                <div class="col-lg-8">
-                                    <input type="text" id="password" name="password" class="form-control">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-lg-8 col-md-offset-4 padding-left-0">
-                                    <a href="#forget-passsword" class="fancybox-fast-view">Forgot Password?</a>
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="col-lg-8 col-md-offset-4 padding-left-0 padding-top-20">
-                                    <input class="btn btn-primary" type="button" Value="Login" onclick="trylogin(); return false;">
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="col-lg-8 col-md-offset-4 padding-left-0 padding-top-20">
-                                    <a href="#registration-form" class="btn btn-primary fancybox-fast-view" type="button">Sign Up</a>
-                                </div>
-                            </div>
-                            <div class="clearfix"></div>
-                        </form>
+
+
+
+<div id="registration-form"class="col-md-12" style="display: none;">
+    <?php echo $this->element('user_info');?>
+</div>
+
+
+
+
+<div id="login-pop-up" style="display:none;">
+    <div class="login-pop-up">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="login-form">
+                <h1>Login</h1>
+                <DIV ID="message" align="center"></DIV>
+                <form role="form" action="<?php echo $this->request->webroot;?>cuisine/login" method="post" class="form-horizontal form-without-legend">
+                    <input type="hidden" name="action" value="login">
+                    <div class="form-group">
+                        <label class="col-lg-4 control-label" for="email">Email <span class="require">*</span></label>
+                        <div class="col-lg-8">
+                            <input type="text" id="email" name="email" class="form-control">
+                        </div>
                     </div>
-
-                </div>
-
+                    <div class="form-group">
+                        <label class="col-lg-4 control-label" for="password">Password <span class="require">*</span></label>
+                        <div class="col-lg-8">
+                            <input type="text" id="password" name="password" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-8 col-md-offset-4 padding-left-0">
+                            <a href="#forget-passsword" class="fancybox-fast-view">Forgot Password?</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="col-lg-8 col-md-offset-4 padding-left-0 padding-top-20">
+                            <input class="btn btn-primary" type="button" Value="Login" onclick="trylogin(); return false;">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="col-lg-8 col-md-offset-4 padding-left-0 padding-top-20">
+                            <a href="#registration-form" class="btn btn-primary fancybox-fast-view" type="button">Sign Up</a>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </form>
             </div>
-        </div>
-        <div id="forget-passsword" style="display: none;">
-            <h1>Forgot Your Password?</h1>
-            <DIV ID="forgot-message" align="center"></DIV>
-            <form role="form" class="form-horizontal form-without-legend" method="post">
-                <div class="form-group col-md-12">
-                    <label class="col-lg-4 control-label" for="email">Email</label>
-                    <div class="col-lg-8">
-                        <input type="hidden" Name="action" value="forgotpass">
-                        <input type="text" Name="Email" id="forgot-email" class="form-control">
-                    </div>
-                </div>
 
-                <div class="col-lg-8 col-md-offset-4 padding-left-0 padding-top-5">
-                    <button class="btn btn-primary" type="button" onclick="forgotpass();">Send</button>
-                </div>
+        </div>
 
-            </form>
-        </div>
-        <div id="registration-form"class="col-md-12" style="display: none;">
-            <?php echo $this->element('user_info');?>
-        </div>
     </div>
 </div>
+
+
+
+<div id="forget-passsword" style="display: none;">
+    <h1>Forgot Your Password?</h1>
+    <DIV ID="forgot-message" align="center"></DIV>
+    <form role="form" class="form-horizontal form-without-legend" method="post">
+        <div class="form-group col-md-12">
+            <label class="col-lg-4 control-label" for="email">Email</label>
+            <div class="col-lg-8">
+                <input type="hidden" Name="action" value="forgotpass">
+                <input type="text" Name="Email" id="forgot-email" class="form-control">
+            </div>
+        </div>
+
+        <div class="col-lg-8 col-md-offset-4 padding-left-0 padding-top-5">
+            <button class="btn btn-primary" type="button" onclick="forgotpass();">Send</button>
+        </div>
+
+    </form>
+</div>
+
+
+
 <!-- END TOP BAR -->
 
 <!-- BEGIN HEADER -->
@@ -175,92 +174,35 @@
 
         <a href="javascript:void(0);" class="mobi-toggler"><i class="fa fa-bars"></i></a>
 
-        <!-- BEGIN CART
-        <div class="top-cart-block">
-            <div class="top-cart-info">
-                <a href="javascript:void(0);" class="top-cart-info-count">3 items</a>
-                <a href="javascript:void(0);" class="top-cart-info-value">$1260</a>
-            </div>
-            <i class="fa fa-shopping-cart"></i>
-
-            <div class="top-cart-content-wrapper">
-                <div class="top-cart-content">
-                    <ul class="scroller" style="height: 250px;">
-                        <li>
-                            <a href="shop-item.html"><img src="<?php echo $this->request->webroot;?>/img/cart-img.jpg" alt="Rolex Classic Watch" width="37" height="34"></a>
-                            <span class="cart-content-count">x 1</span>
-                            <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
-                            <em>$1230</em>
-                            <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
-                        </li>
-                        <li>
-                            <a href="shop-item.html"><img src="<?php echo $this->request->webroot;?>/img/cart-img.jpg" alt="Rolex Classic Watch" width="37" height="34"></a>
-                            <span class="cart-content-count">x 1</span>
-                            <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
-                            <em>$1230</em>
-                            <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
-                        </li>
-                        <li>
-                            <a href="shop-item.html"><img src="<?php echo $this->request->webroot;?>/img/cart-img.jpg" alt="Rolex Classic Watch" width="37" height="34"></a>
-                            <span class="cart-content-count">x 1</span>
-                            <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
-                            <em>$1230</em>
-                            <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
-                        </li>
-                        <li>
-                            <a href="shop-item.html"><img src="<?php echo $this->request->webroot;?>/img/cart-img.jpg" alt="Rolex Classic Watch" width="37" height="34"></a>
-                            <span class="cart-content-count">x 1</span>
-                            <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
-                            <em>$1230</em>
-                            <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
-                        </li>
-                        <li>
-                            <a href="shop-item.html"><img src="<?php echo $this->request->webroot;?>/img/cart-img.jpg" alt="Rolex Classic Watch" width="37" height="34"></a>
-                            <span class="cart-content-count">x 1</span>
-                            <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
-                            <em>$1230</em>
-                            <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
-                        </li>
-                        <li>
-                            <a href="shop-item.html"><img src="<?php echo $this->request->webroot;?>/img/cart-img.jpg" alt="Rolex Classic Watch" width="37" height="34"></a>
-                            <span class="cart-content-count">x 1</span>
-                            <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
-                            <em>$1230</em>
-                            <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
-                        </li>
-                        <li>
-                            <a href="shop-item.html"><img src="<?php echo $this->request->webroot;?>/img/cart-img.jpg" alt="Rolex Classic Watch" width="37" height="34"></a>
-                            <span class="cart-content-count">x 1</span>
-                            <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
-                            <em>$1230</em>
-                            <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
-                        </li>
-                        <li>
-                            <a href="shop-item.html"><img src="<?php echo $this->request->webroot;?>/img/cart-img.jpg" alt="Rolex Classic Watch" width="37" height="34"></a>
-                            <span class="cart-content-count">x 1</span>
-                            <strong><a href="shop-item.html">Rolex Classic Watch</a></strong>
-                            <em>$1230</em>
-                            <a href="javascript:void(0);" class="del-goods">&nbsp;</a>
-                        </li>
-                    </ul>
-                    <div class="text-right">
-                        <a href="shop-shopping-cart.html" class="btn btn-default">View Cart</a>
-                        <a href="shop-checkout.html" class="btn btn-primary">Checkout</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        END CART -->
-
         <!-- BEGIN NAVIGATION -->
         <div class="header-navigation">
             <ul>
 
                 <li><a href="<?php echo $this->request->webroot;?>">Home</a></li>
-                <li><a href="<?php echo $this->request->webroot;?>restaurants/all">List Restaurants</a></li>
+                <li><a href="<?php echo $this->request->webroot;?>restaurants/all">Restaurants</a></li>
                 <li><a href="<?php echo $this->request->webroot;?>restaurants/signup">Restaurant Sign Up</a></li>
-                <li><a href="<?php echo $this->request->webroot;?>pages/about">About Us</a></li>
                 <li><a href="<?php echo $this->request->webroot;?>pages/contact">Contact Us</a></li>
+
+
+                <?php
+                    $userName = "Not logged in";
+                    $Restaurant = $userName;
+                    if($Profile) {
+                        $userName = ucfirst($Profile->Name);
+                        $Restaurant = $Profile->RestaurantID;
+                    }
+                ?>
+                <!-- BEGIN TOP BAR MENU -->
+                    <?php
+                        if($userID){
+                            //echo '<li><a href="shop-account.html">My Account</a></li>';
+                            echo '<li><a href="' . $this->request->webroot . 'users/dashboard">' . $userName . "'s Dashboard</a></li>";
+                            echo '<li><a href="' . $this->request->webroot . 'users/logout">Log Out</a></li>';
+                        } else {
+                            echo '<li><a href="#login-pop-up" class="fancybox-fast-view">Log In</a></li>';
+                        }
+                    ?>
+
 
                 <!-- BEGIN TOP SEARCH -->
                 <li class="menu-search">
@@ -271,7 +213,7 @@
                             <div class="input-group" valign="center">
                                 <input type="text" placeholder="Search" class="form-control">
                                 <span class="input-group-btn">
-                                    <button class="btn btn-primary"  type="submit">Search</button>
+                                    <button class="btn btn-primary" type="submit">Search</button>
                                 </span>
                             </div>
                         </form>
